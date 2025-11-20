68ff4ebf09e9c262984bd903427f257736aa2007
prasun_tuladhar <np01cp4a250025@islingtoncollege.edu.np>
2025-11-20 16:20:18 +0545

    javaaaaaa  work

---
+++ Rikshaw.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+/**
+ * Write a description of class Rikshaw here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Rikshaw
+{
+    public static void main(String[] args)
+    {
+        Scanner input= new Scanner(System.in);
+        System.out.println("What is the distance?");
+        int distance = input.nextInt();
+        System.out.println("Are you a local?\n 1 for yes, 0 for no");
+        int answer=input.nextInt();
+        boolean local;
+        boolean day;
+        if(answer == 1)
+        {
+            local=true;
+        }
+        else;
+        {
+            
+            local=false;
+        }
+        System.out.println("Its day or night?\n 1 for day, 0 for night");
+        int answer1= input.nextInt();
+        if(answer1==1)
+        {
+            day = true;
+        }
+        else;
+        {
+            day= false;
+        }
+        float price_multiplier=1;
+        float price_multiplier= 1.5f;
+        if(day)
+        {
+            price_multiplier =1;
+        }
+    }
+}
